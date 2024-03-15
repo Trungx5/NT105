2b3938a81c014b05c622d17b64fff7ea83cede24
Trungx5 <22521554@gm.uit.edu.vn>
2024-03-15 22:57:57 +0700

    Lab01_NT105

---
+++ NT105/Lab01/Bai02.m
@@ -0,0 +1,13 @@
+A = 4; B = 5;
+C = 6; D = 7;
+
+
+SoPhuc1 = complex(A,B);
+SoPhuc2 = complex(C,D);
+
+PhepCong = SoPhuc1 + SoPhuc2;
+PhepTru = SoPhuc1 - SoPhuc2;
+PhepNhan = SoPhuc1 * SoPhuc2;
+PhepChia = SoPhuc1 / SoPhuc2;
+
+%patch lỗi số Chia Tử 0%
